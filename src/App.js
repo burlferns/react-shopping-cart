cea62bc9ca05ca1a8dfcf78f238227470ea25034
std <std@admins-Mac-mini.local>
2019-11-04 17:19:45 -0800

    completed MVP & first stretch

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 // Contexts
 import {ProductContext} from './contexts/ProductContext';
+import {CartContext} from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -20,21 +21,26 @@ function App() {
       ...cart,
       item,
     ]);
-	};
+  };
+  
+  const removeItem = item => {
+    const newCart = cart.filter(cartItem=>item.id!==cartItem.id);   
+		setCart(newCart);
+  };
+
 
 	return (
 		<div className="App">
+      <CartContext.Provider value={{cart,removeItem}}>
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
+        <Navigation />
 
         {/* Routes */}
         <Route exact path="/" component={Products} />
+        <Route path="/cart" component={ShoppingCart} />
 
-        <Route
-          path="/cart"
-          render={() => <ShoppingCart cart={cart} />}
-        />
       </ProductContext.Provider>
+      </CartContext.Provider>
 		</div>
 	);
 }
